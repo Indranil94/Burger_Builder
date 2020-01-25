670295cd403669821250ca68bc0844ec062901c5
Indranil94 <indra.santra@gmail.com>
2020-01-25 16:13:29 +0530

    #1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Layout from './components/Layout/layout'
 
 
 function App() {
   return (
     <div>
-      
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
